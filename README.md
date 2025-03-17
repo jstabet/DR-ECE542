add375bf14dfab91d2907709159b86d9dd48095c
Jimmy Tabet <jimmy_tabet@med.unc.edu>
2025-03-17 16:34:59 -0400

    added eda, transfer_learning, utils scripts and data/initial resnet results. EDA and full training WIP

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # DR-ECE542
 Diabetic retinopathy detection for ECE542 course project
 
+## Conda Environment (WIP)
+```conda create -n dr torch torchvision pillow pandas numpy matplotlib scikit-learn```
+
 ## Dataset
 [Download from Kaggle](https://www.kaggle.com/competitions/diabetic-retinopathy-detection/data)
 >Note: to access training images, unzip into train.zip.00X then run
@@ -8,3 +11,10 @@ Diabetic retinopathy detection for ECE542 course project
 >cat train.zip.001 train.zip.002 train.zip.003 train.zip.004 train.zip.005 > train.zip
 >unzip train.zip
 >```
+
+### Labels  
+0 - No DR  
+1 - Mild  
+2 - Moderate  
+3 - Severe  
+4 - Proliferative DR  
---
+++ src/utils.py
@@ -0,0 +1,45 @@
+import torch
+from PIL import Image
+import os
+
+class DRDataset(torch.utils.data.Dataset):
+    '''
+    Dataset class for DR images.
+    '''
+
+    def __init__(self, label_df, images_source, preproc=None):
+
+        # set internal variables
+        self.label_df = label_df
+        self.preproc = preproc
+        
+        # load images into memory if images_source is a file
+        if os.path.isfile(images_source):
+            self.images_source = torch.load(images_source)
+            self.mem = True
+        # set images_source as directory if it is a directory
+        else:
+            assert os.path.isdir(images_source)
+            self.images_source = images_source
+            self.mem = False
+    
+    def __getitem__(self, idx):
+
+        # get subset based on idx
+        subset = self.label_df.iloc[idx]
+        img_path = subset['image'] + '.jpeg'
+        img = Image.open(os.path.join(self.images_source, img_path))
+        label = subset['level']
+        
+        # load image if in memory
+        if self.mem:
+            img_preproc = self.images_source[subset.name] # subset.name is original index of entire df
+
+        # OR preprocess image
+        else:
+            img_preproc = self.preproc(img)
+
+        return img_preproc, label
+    
+    def __len__(self):
+        return len(self.label_df)
